f17cbf9352b83a8e1edfc4380793b31dfb59b230
theabsparrow <absbashar04@gmail.com>
2024-08-15 11:14:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>  
+      <div className="flex justify-center">
+        <button className="btn btn-primary">click here</button>
+      </div>
+    </>
+  )
+}
+
+export default App
